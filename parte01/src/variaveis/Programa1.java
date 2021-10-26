0961ac0b163ab88f306a5edde028d2260991cb84
Antonio A Alves <antonioaugusto.alves1981@gmail.com>
2021-10-26 08:52:01 -0300

    Tipos de dados em Java

--- parte01/src/variaveis/Programa1.java
+++ parte01/src/variaveis/Programa1.java
@@ -8,11 +8,21 @@ public class Programa1 {
 		int v3 = 677;
 		long v4 = 678088;
 		
+		double v5 = 76.88;
+		float v6 = 75.55F;
+		
+		boolean isPresent = false;
+		char letra = 'd';
+		
 
 		System.out.println(v1);
 		System.out.println(v2);
 		System.out.println(v3);
 		System.out.println(v4);
+		System.out.println(v5);
+		System.out.println(v6);
+		System.out.println(isPresent);
+		System.out.println(letra);
 		
 
 	}
