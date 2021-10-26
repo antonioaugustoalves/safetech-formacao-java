b4eb754d941bd635258ea8eb2ac7fc7ca94ed91d
Antonio A Alves <antonioaugusto.alves1981@gmail.com>
2021-10-26 08:35:55 -0300

    Commit inicial

---
+++ parte01/src/variaveis/Programa1.java
@@ -0,0 +1,20 @@
+package variaveis;
+
+public class Programa1 {
+
+	public static void main(String[] args) {
+		byte v1 = 7;
+		short v2 = 34;
+		int v3 = 677;
+		long v4 = 678088;
+		
+
+		System.out.println(v1);
+		System.out.println(v2);
+		System.out.println(v3);
+		System.out.println(v4);
+		
+
+	}
+
+}
